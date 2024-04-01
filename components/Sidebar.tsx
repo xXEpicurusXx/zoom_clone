dcbfa5e59f779b1ab15919e9b1664baaddf2de11
Cameron Mckeel <117554357+xXEpicurusXx@users.noreply.github.com>
2024-04-01 11:27:54 +0900

    working on page routing and colors

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,7 @@
 
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
@@ -27,7 +28,15 @@ const Sidebar = () => {
                 }
               )}
             >
-              {link.label}
+              <Image
+                src={link.imgUrl}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-lg font-semibold max-lg:hidden">
+                {link.label}
+              </p>
             </Link>
           );
         })}
