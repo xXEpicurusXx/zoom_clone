cc0e0d06bcd5b472e880e5ba5bc1724dd249cc42
Cameron Mckeel <117554357+xXEpicurusXx@users.noreply.github.com>
2024-04-01 14:50:04 +0900

    added cards for different meeting functionality

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import MeetingTypeList from "@/components/MeetingTypeList";
+import React from "react";
 
-const Home = () => {
-  return (
-    <section className= 'flex size-full flex-col gap-10 text-white'>
-      <h1 className='text-3xl font-bold'>Home</h1>
-
-      
-    </section>
-  )
+interface TimeOptions {
+  hour12: boolean;
+  hour: "numeric" | "2-digit";
+  minute: "numeric" | "2-digit";
+  hourCycle: "h12";
 }
 
-export default Home
+const Home = () => {
+  const now = new Date();
+  const options: TimeOptions = {
+    hour12: true,
+    hour: "numeric",
+    minute: "2-digit",
+    hourCycle: "h12",
+  };
+
+  const time = now.toLocaleTimeString("en-US", options);
+  const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+    now
+  );
+
+  return (
+    <section className="flex size-full flex-col gap-5 text-white">
+      <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
+            Upcoming Meeting at: 12:30 PM
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+          </div>
+        </div>
+      </div>
+  
+      <MeetingTypeList />
+    </section>
+  );
+};
+
+export default Home;
+
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,34 @@
-import Image from '@/node_modules/next/image'
-import Link from '@/node_modules/next/link'
-import React from 'react'
-import MobileNav from './MobileNav'
+import Image from "@/node_modules/next/image";
+import Link from "@/node_modules/next/link";
+import React from "react";
+import MobileNav from "./MobileNav";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
-    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
-      <Link href="/" className='flex items-center gap-1'>
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
         <Image
-        src="/icons/logo.svg"
-        alt='Yoom Logo'
-        width={32}
-        height={32}
-        className="max-sm:10"
+          src="/icons/logo.svg"
+          alt="Yoom Logo"
+          width={32}
+          height={32}
+          className="max-sm:10"
         />
-        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Yoom</p>
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Yoom
+        </p>
       </Link>
 
-      <div className='flex-between gap-5'>
-        {/* CLerk user management */}
+      <div className="flex-between gap-5">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
 
-        <MobileNav/>
+        <MobileNav />
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
