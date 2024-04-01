fb8881cd206ac94aa51ef62dc148e819ea3cf2c3
Cameron <cameron_mckeel@yahoo.com>
2024-04-01 11:42:14 +0900

    moblie navbar creation

---
+++ components/MobileNav.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MobileNav = () => {
+  return (
+    <section className='w-full max-w-[264px]'></section>
+  )
+}
+
+export default MobileNav
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,28 @@
+import Image from '@/node_modules/next/image'
+import Link from '@/node_modules/next/link'
 import React from 'react'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+      <Link href="/" className='flex items-center gap-1'>
+        <Image
+        src="/icons/logo.svg"
+        alt='Yoom Logo'
+        width={32}
+        height={32}
+        className="max-sm:10"
+        />
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Yoom</p>
+      </Link>
+
+      <div className='flex-between gap-5'>
+        {/* CLerk user management */}
+
+        <MobileNav/>
+      </div>
+    </nav>
   )
 }
 
